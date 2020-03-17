9a6a05888bc82f58ff461e04b0128a375a809078
thibautgallice <gt.thibaut@gmail.com>
2020-03-17 19:22:05 +0100

    action mailer with all email conditions (still in user.rb => spread needed)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,61 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  after_create :welcome_to_user_email
+  after_create :new_user_register_to_admin_email
+
+  # ========= START MAILER METHODS ========= 
+
+  # TO USER
+  # Welcome email
+  def welcome_to_user_email
+    UserMailer.welcome_to_user_email(self).deliver_now
+  end
+  
+  # TO ADMIN
+  # New user subscribe mail
+  def new_user_register_to_admin_email
+    AdminMailer.new_user_register_to_admin_email(self).deliver_now
+  end
+
+  # ========= END MAILER ========= 
+
+
+# # DANS LE MODEL GROUP
+#   after_create :create_group_to_admin_email
+#   after_create :user_create_group_to_grp_admin_email
+  
+#   def create_group_to_admin_email
+#     AdminMailer.create_group_to_admin_email(self).deliver_now
+#   end
+
+#   def user_create_group_to_grp_admin_email
+#     UserMailer.user_create_group_to_grp_admin_email(self).deliver_now
+#   end
+
+# # DANS LE MODEL SUBSCRIPTION
+#   after_create :new_user_subscribe_a_group_to_admin_email
+#   after_create :new_user_subscribe_alert_to_grp_users_email
+#   after_create :user_subscribe_group_to_user_email
+
+#   def new_user_subscribe_a_group_to_admin_email
+#     AdminMailer.new_user_subscribe_a_group_to_admin_email(self).deliver_now
+#   end
+
+#   def new_user_subscribe_alert_to_grp_users_email
+#     UserMailer.new_user_subscribe_alert_to_grp_users_email(self).deliver_now
+#   end
+
+#   def user_subscribe_group_to_user_email
+#     UserMailer.user_subscribe_group_to_user_email(self).deliver_now
+#   end
+
+# # DANS LE MODEL COMMENT
+#   after_create :new_comment_alert_to_grp_users_email
+
+#   def new_comment_alert_to_grp_users_email
+#     UserMailer.new_comment_alert_to_grp_users_email(self).deliver_now
+#   end
+
+
 end
