05b870b865e525ca6269229199638986b7924002
thibautgallice <gt.thibaut@gmail.com>
2020-03-18 13:57:46 +0100

    api before reorder following Yanno advice

--- app/controllers/udemy_controller.rb
+++ app/controllers/udemy_controller.rb
@@ -1,28 +1,32 @@
 class UdemyController < ApplicationController
 
-  @baseUrl="https://www.udemy.com/api-2.0"
+  @@baseUrl="https://www.udemy.com/api-2.0"
 
-  @headers = {
-    "Accept": "application/json, text/plain, */*",
-    "Authorization": "Basic ***REMOVED***",
-    "Content-Type": "application/json;charset=utf-8"
-  }
-  
-  def query(path)
-    url = @baseUrl + path
-    response = RestClient.get(url, headers=@headers)
-    return response
-  end
 
-  def getRubyCourses
-    response = query("/courses/?page_size=10&search=ruby")
-    json = JSON.parse(response.body)
-    return json['results']
-  end
 
   def index
     @rubyCourses = getRubyCourses
   end 
+
+  private
+
+  def query(path)
+    headers = {
+    "Accept": "application/json, text/plain, */*",
+    "Authorization": "Basic ***REMOVED***",
+    "Content-Type": "application/json;charset=utf-8"
+    }
+    
+    url = @@baseUrl + path
+    response = RestClient.get(url, headers=headers)
+    return response
+  end
+
+  def getRubyCourses
+    response = query("/courses/?page_size=20&search=ruby")
+    json = JSON.parse(response.body)
+    return json['results']
+  end
 
   
   
