35dd0f2b322f1f44b1b8a975aee125cd4adc8d68
Le Lan-Toson <clelantoson@hotmail.fr>
2020-03-19 10:05:35 +0100

    deleted useless route

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,4 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'groups#index'
-  get 'ruby/index', to: 'udemy#index'
 end
