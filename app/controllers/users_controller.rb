f1186104ced233a2146ab22b5f876e9ad2d30f76
Le Lan-Toson <clelantoson@hotmail.fr>
2020-03-20 20:20:14 +0100

    added cards for groups and subscriptions of user in profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id:params[:id])
-    # @user_sub = Subscription.where(user:@user,group_id:@group.id).exists?
-    # @group_subs = Subscription.where(group_id:params[:id])
-
-
     if current_user == @user
       @user = User.find_by(id:params[:id])
     else
