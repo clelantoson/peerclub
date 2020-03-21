54b936706ab7494e9935fbe3cc3697750b58a89c
thibautgallice <gt.thibaut@gmail.com>
2020-03-21 11:51:37 +0100

    fix responsive issue on whychoose section + js hover for group card

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,18 @@
 //= require turbolinks
 //= require rails-ujs
 
+$(document).ready(function() {
+ // executes when HTML-Document is loaded and DOM is ready
+console.log("document is ready");
+  
+
+  $( ".card" ).hover(
+  function() {
+    $(this).addClass('shadow-lg').css('cursor', 'pointer'); 
+  }, function() {
+    $(this).removeClass('shadow-lg');
+  }
+);
+  
+// document ready  
+});
