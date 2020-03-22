d1ceef4cd36c7656cf522af90c04d8efbfe4be08
thibautgallice <gt.thibaut@gmail.com>
2020-03-22 16:04:21 +0100

    front group show

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,7 +26,7 @@ console.log("document is ready");
 
   $( ".card" ).hover(
   function() {
-    $(this).addClass('shadow-lg').css('cursor', 'pointer'); 
+    $(this).addClass('shadow-lg'); 
   }, function() {
     $(this).removeClass('shadow-lg');
   }
