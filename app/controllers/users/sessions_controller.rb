a817d010fdb33bb333ac90ed39e5eb07c6504083
thibautgallice <gt.thibaut@gmail.com>
2020-03-22 12:17:25 +0100

    flash messages done

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -20,7 +20,7 @@ class Users::SessionsController < Devise::SessionsController
   # DELETE /resource/sign_out
   def destroy
     signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
-    set_flash_message! :danger, :signed_out if signed_out
+    set_flash_message! :info, :signed_out if signed_out
     yield if block_given?
     respond_to_on_destroy
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :groups do
     resources :subscriptions
   end
-  devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
+  devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions', confirmations: 'users/confirmations', passwords: 'users/passwords', unlocks: 'users/unlocks' }
 
   resources :users, only: [:show]
   resources :comments, only: [:new, :create, :show, :destroy]
