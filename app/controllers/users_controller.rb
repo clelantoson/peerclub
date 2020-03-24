323c0fe40fda285b2604ebc9577f34cdcecacc4f
thibautgallice <gt.thibaut@gmail.com>
2020-03-24 12:17:21 +0100

    prod ready

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,13 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
 
   def show
     @user = User.find_by(id:params[:id])
-    if current_user == @user
-      @user = User.find_by(id:params[:id])
-    else
-      redirect_to root_path
-    end
+    # if current_user == @user
+    #   @user = User.find_by(id:params[:id])
+    # else
+    #   redirect_to root_path
+    # end
 
   end
 end
