d8f6b80c9ef2b9eb2cd86e7c1ceabe455e7b06f3
thibautgallice <gt.thibaut@gmail.com>
2020-03-27 14:42:21 +0100

    misc fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,6 +19,18 @@
 //= require rails-ujs
 //= require toastr
 
+(function() {
+    "use strict";
+    if (window.location && window.location.hash) {
+        if (window.location.hash === '#_=_') {
+            window.location.hash = '';
+            return;
+        }
+        var facebookFubarLoginHash = RegExp('_\=_', 'g');
+        window.location.hash = window.location.hash.replace(facebookFubarLoginHash,     '');
+    }
+}()); 
+
 $(document).ready(function() {
  // executes when HTML-Document is loaded and DOM is ready
 console.log("document is ready");
